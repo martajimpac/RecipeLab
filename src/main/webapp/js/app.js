5e78d79435c56b5716bad1b4fda751e3b8d0971e
Marta Jiménez Pacheco <martajimpac@gmail.com>
2023-04-17 01:27:30 +0200

    subir el script

---
+++ src/main/webapp/js/app.js
@@ -0,0 +1,18 @@
+var loginImage = document.getElementById("login-image");
+var submenu = document.querySelector(".submenu");
+var cerrarSesion = document.getElementById("cerrar-sesion");
+
+loginImage.addEventListener("click", function() {
+    console.log(submenu)
+    submenu.style.display = "block";
+});
+
+document.addEventListener("click", function(event) {
+    if (!submenu.contains(event.target) && event.target !== loginImage) {
+        submenu.style.display = "none";
+    }
+});
+
+cerrarSesion.addEventListener("click", function() {
+    alert("La sesion se ha cerrado correctamente")
+});
